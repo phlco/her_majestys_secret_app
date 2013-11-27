e85fd4dc5e3b9931b1564784787df2b563caeb4d
Will Smith <will.smith@preplaysports.com>
2013-11-27 15:47:24 -0500

    all green

--- app/assets/javascripts/underscore_films.js
+++ app/assets/javascripts/underscore_films.js
@@ -24,3 +24,114 @@ bonds.films = [
   { title: "A View to a Kill", year: 1985, actor: "Roger Moore", gross: "$321,172,633" },
   { title: "Licence to Kill", year: 1989, actor: "Timothy Dalton", gross: "$285,157,191" }
 ];
+
+bonds.gross = function( film ) {
+  var num = film.gross.slice(1).split(',').join('');
+  return parseInt(num);
+};
+
+bonds.getActors = function(){
+  var actors = _.map(this.films, function(film){
+    return film.actor;
+  });
+  return _.uniq(actors);
+};
+
+bonds.totalGross = function(){
+  var grossArray = _.map(this.films, function(film){ return bonds.gross(film); } );
+  var gross = _.reduce(grossArray, function(memo, num){ return memo + num; }, 0);
+  return gross;
+};
+
+bonds.titles = function( options ){
+  var titles = _.map(this.films, function(film){
+    return film.title;
+  });
+  var titleWordCount = [];
+  for (var i = 0; i<titles.length;i++){
+    if (options.words === titles[i].split(' ').length) {
+      titleWordCount.push(titles[i]);
+    }
+  }
+  return titleWordCount;
+};
+
+bonds.starCount = function(){
+  var actors = _.countBy(this.films, function(film){
+    return film.actor;
+  });
+  return actors;
+};
+
+bonds.loneliestBond = function(){
+  var actors = _.groupBy(this.films, function(film){
+    return film.actor;
+  });
+  var filmCount = [];
+  for(var key in actors){
+    var value = actors[key];
+    filmCount.push(value);
+  }
+  var filmSize = [];
+  for (var i = 0; i<filmCount.length; i++){
+    var size = _.size(filmCount[i]);
+    filmSize.push(size);
+  }
+  var minFilmSize = _.min(filmSize);
+  for (var p = 0; p<filmCount.length; p++){
+    if (minFilmSize == _.size(filmCount[p])){
+      return (filmCount[p][0].actor);
+    }
+  }
+};
+
+bonds.oddBonds = function(){
+  var years = _.map(this.films, function(film){
+    return film.year;
+  });
+  var odds = _.filter(years, function(num){
+    return num % 2 !== 0;
+  });
+  var oddYears = [];
+  for (var i = 0; i<odds.length; i++){
+    var filmByYear = _.findWhere(this.films, { year: odds[i] });
+    oddYears.push(filmByYear.title);
+  }
+  return oddYears;
+};
+
+bonds.bestBond = function(){
+  return _.max(
+    _.map(_.groupBy(bonds.films, "actor"), function(films, actor){
+      var numberOfFilms = films.length;
+      var grosses = _.map(films, function(film){
+        return bonds.gross(film);
+      });
+      var totalGross = _.reduce(grosses, function(memo, num){
+        return memo + num;
+      });
+      var average = totalGross/numberOfFilms;
+      return { actor: actor, gross: average };
+    }), function(actor){
+      return actor.gross;
+    }
+  );
+};
+
+bonds.worstBond = function(){
+  return _.min(
+    _.map(_.groupBy(bonds.films, "actor"), function(films, actor){
+      var numberOfFilms = films.length;
+      var grosses = _.map(films, function(film){
+        return bonds.gross(film);
+      });
+      var totalGross = _.reduce(grosses, function(memo, num){
+        return memo + num;
+      });
+      var average = totalGross/numberOfFilms;
+      return { actor: actor, gross: average };
+    }), function(actor){
+      return actor.gross;
+    }
+  );
+};
