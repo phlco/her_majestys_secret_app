0df9e777d5bbd907ed7a227105b1331c74bec5f7
Katherine Santiago <santiago.kat@gmail.com>
2013-12-02 12:28:38 -0500

    updated bond

--- spec/javascripts/backbone_films_spec.js
+++ spec/javascripts/backbone_films_spec.js
@@ -7,7 +7,7 @@ describe("model and collection", function(){
 
     describe(".gross()", function(){
       it("converts gross to a number", function(){
-        expect( film.gross() ).toEqual(1108561008);
+        expect( film.gross() ).toEqual(756544419);
       });
     });
 
@@ -15,7 +15,7 @@ describe("model and collection", function(){
 
   describe("Films", function(){
 
-    var films = new Films( bond.films );
+    var films = new Films( bonds.films );
 
     describe(".getActors()", function(){
       it("returns all the Actors' names without duplicates", function(){
